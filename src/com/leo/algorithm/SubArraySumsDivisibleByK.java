912fa234cdf94c9256af679ac389ff3b7cf21841
Leo.ZhangTJ <Leo.ZhangTJ@homecreditcfc.cn>
2020-05-27 18:04:06 +0800

    1.子数组求K 求K余数

--- src/com/leo/algorithm/SubArraySumsDivisibleByK.java
+++ src/com/leo/algorithm/SubArraySumsDivisibleByK.java
@@ -1,5 +1,7 @@
 package com.leo.algorithm;
 
+import java.util.HashMap;
+
 /**
  * https://leetcode-cn.com/problems/subarray-sums-divisible-by-k/
  */
@@ -8,7 +10,7 @@ public class SubArraySumsDivisibleByK {
     public static void main(String[] args) {
         int[] nums = {4, 5, 0, -2, -3, 1};
         int k = 5;
-        int reulst = subarraysDivByK(nums, k);
+        int reulst = subarraysDivByK2(nums, k);
         System.out.println(reulst);
     }
 
@@ -27,6 +29,24 @@ public class SubArraySumsDivisibleByK {
         return result;
     }
 
+    //前缀 hash求解
+    public static int subarraysDivByK2(int[] A, int K) {
+        int result = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0;
+        int m = 0;
+        for (int value : A) {
+            sum += value;
+            m = sum % K;
+            if (m < 0) {
+                m += K;
+            }
+            map.put(m, map.getOrDefault(m, 0) + 1);
+            result += map.get(m) - 1;
+        }
+        return result;
+    }
 
 
 }
