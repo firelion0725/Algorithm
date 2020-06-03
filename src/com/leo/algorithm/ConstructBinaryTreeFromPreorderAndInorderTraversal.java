9e464f9b66e34d0c5c64a6fba8240c653317aa01
Leo.ZhangTJ <Leo.ZhangTJ@homecreditcfc.cn>
2020-06-03 14:51:51 +0800

    1.构建二叉树

--- src/com/leo/algorithm/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ src/com/leo/algorithm/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -3,6 +3,8 @@ package com.leo.algorithm;
 import com.leo.algorithm.model.TreeNode;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
 
@@ -11,19 +13,20 @@ public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
 //        int[] inorder = {9, 3, 15, 20, 7};
         int[] preorder = {1, 2, 3};
         int[] inorder = {3, 2, 1};
-        TreeNode tree = buildTree(preorder, inorder);
+        TreeNode tree = buildTreeOffice(preorder, inorder);
     }
 
 
     public static TreeNode buildTree(int[] preorder, int[] inorder) {
-        return helper(null, preorder, inorder);
+        return helper(preorder, inorder);
     }
 
-    private static TreeNode helper(TreeNode tree, int[] preorder, int[] inorder) {
+    private static TreeNode helper(int[] preorder, int[] inorder) {
         if (preorder.length == 0 || inorder.length == 0) {
             return null;
         }
 
+        TreeNode tree;
         if (preorder.length == 1) {
             tree = new TreeNode(preorder[0]);
             return tree;
@@ -36,14 +39,14 @@ public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
         if (inorderLeft.length <= 1) {
             tree.left = inorderLeft.length == 1 ? new TreeNode(inorderLeft[0]) : null;
         } else {
-            int[] preorderLeft = Arrays.copyOfRange(preorder, 1, headIndex+1);
-            tree.left = helper(tree, preorderLeft, inorderLeft);
+            int[] preorderLeft = Arrays.copyOfRange(preorder, 1, headIndex + 1);
+            tree.left = helper(preorderLeft, inorderLeft);
         }
         if (inorderRight.length <= 1) {
             tree.right = inorderRight.length == 1 ? new TreeNode(inorderRight[0]) : null;
         } else {
             int[] preorderRight = Arrays.copyOfRange(preorder, headIndex + 1, preorder.length);
-            tree.right = helper(tree.right, preorderRight, inorderRight);
+            tree.right = helper(preorderRight, inorderRight);
         }
 
         return tree;
@@ -58,5 +61,45 @@ public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
         return 0;
     }
 
+    //======================================OFFICE==============================================================
+
+    /**
+     * @param indexMap       中序数组 值为K 下表为V
+     * @param preorder       前序数组
+     * @param preorder_left  前序左边界
+     * @param preorder_right 前序右边界
+     * @param inorder_left   中序左边界
+     * @return 生成的树
+     */
+    public static TreeNode helper(Map<Integer, Integer> indexMap, int[] preorder, int preorder_left, int preorder_right, int inorder_left) {
+        if (preorder_left > preorder_right) {
+            return null;
+        }
+
+        // 通过前序的值获取中序的位置
+        int inorder_root = indexMap.get(preorder[preorder_left]);
+
+        // 先把根节点建立出来
+        TreeNode root = new TreeNode(preorder[preorder_left]);
+
+        // 得到左子树中的节点数目
+        int size_left_subtree = inorder_root - inorder_left;
+        // 递归地构造左子树，并连接到根节点
+        // 先序遍历中「从 左边界+1 开始的 size_left_subtree」个元素就对应了中序遍历中「从 左边界 开始到 根节点定位-1」的元素
+        root.left = helper(indexMap, preorder, preorder_left + 1, preorder_left + size_left_subtree, inorder_left);
+        // 递归地构造右子树，并连接到根节点
+        // 先序遍历中「从 左边界+1+左子树节点数目 开始到 右边界」的元素就对应了中序遍历中「从 根节点定位+1 到 右边界」的元素
+        root.right = helper(indexMap, preorder, preorder_left + size_left_subtree + 1, preorder_right, inorder_root + 1);
+        return root;
+    }
+
+    public static TreeNode buildTreeOffice(int[] preorder, int[] inorder) {
+        // 构造哈希映射，帮助我们快速定位根节点
+        Map<Integer, Integer> indexMap = new HashMap<>();
+        for (int i = 0; i < preorder.length; i++) {
+            indexMap.put(inorder[i], i);
+        }
+        return helper(indexMap, preorder, 0, preorder.length - 1, 0);
+    }
 
 }
