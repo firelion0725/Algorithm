49ea4855f7d07c336c9789d18ccb9e80daec1b96
Leo <firelion0725@163.com>
2020-06-12 14:23:03 +0800

    1. 岛屿问题

--- src/com/leo/algorithm/NumIslands.java
+++ src/com/leo/algorithm/NumIslands.java
@@ -7,16 +7,17 @@ public class NumIslands {
         if (grid == null || grid.length == 0) {
             return 0;
         }
-
+        //纵坐标系
         int height = grid.length;
+        //横坐标系
         int width = grid[0].length;
         int result = 0;
 
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                if (grid[j][i] == '1') {
-                    dfs(grid, j, i, width, height);
+                if (grid[i][j] == '1') {
                     result++;
+                    dfs(grid, j, i, width, height);
                 }
             }
         }
@@ -26,15 +27,15 @@ public class NumIslands {
 
     private void dfs(char[][] grid, int w, int h, int width, int height) {
 
-        if (w < 0 || h < 0 || w >= width || h >= height || grid[w][h] == '0') {
+        if (w < 0 || h < 0 || w >= width || h >= height || grid[h][w] == '0') {
             return;
         }
 
-        grid[w][h] = '0';
-        dfs(grid, w, h, width - 1, height);
-        dfs(grid, w, h, width + 1, height);
-        dfs(grid, w, h, width, height - 1);
-        dfs(grid, w, h, width, height + 1);
+        grid[h][w] = '0';
+        dfs(grid, w + 1, h, width, height);
+        dfs(grid, w - 1, h, width, height);
+        dfs(grid, w, h + 1, width, height);
+        dfs(grid, w, h - 1, width, height);
 
     }
 
