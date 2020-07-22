177bda8e0d08facffba5ca56c51c07bac94fd7b9
Leo <firelion0725@163.com>
2020-07-22 14:47:42 +0800

    生成二叉搜索树

---
+++ src/com/leo/algorithm/GenerateTrees.java
@@ -0,0 +1,50 @@
+package com.leo.algorithm;
+
+import com.leo.algorithm.model.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class GenerateTrees {
+
+    public static void main(String[] args) {
+        GenerateTrees g = new GenerateTrees();
+        List<TreeNode> list = g.generateTrees(3);
+        System.out.println(list);
+    }
+
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new LinkedList<TreeNode>();
+        }
+        return generateTrees(1, n);
+    }
+
+    public List<TreeNode> generateTrees(int start, int end) {
+        List<TreeNode> allTrees = new LinkedList<TreeNode>();
+        if (start > end) {
+            allTrees.add(null);
+            return allTrees;
+        }
+
+        // 枚举可行根节点
+        for (int i = start; i <= end; i++) {
+            // 获得所有可行的左子树集合
+            List<TreeNode> leftTrees = generateTrees(start, i - 1);
+
+            // 获得所有可行的右子树集合
+            List<TreeNode> rightTrees = generateTrees(i + 1, end);
+
+            // 从左子树集合中选出一棵左子树，从右子树集合中选出一棵右子树，拼接到根节点上
+            for (TreeNode left : leftTrees) {
+                for (TreeNode right : rightTrees) {
+                    TreeNode currTree = new TreeNode(i);
+                    currTree.left = left;
+                    currTree.right = right;
+                    allTrees.add(currTree);
+                }
+            }
+        }
+        return allTrees;
+    }
+}
