fe4c86f1fb0fae81b9d832ef23a98c33b0b10448
Leo <firelion0725@163.com>
2020-09-07 13:44:37 +0800

    141. 环形链表

---
+++ src/com/leo/algorithm/HasCycle.java
@@ -0,0 +1,26 @@
+package com.leo.algorithm;
+
+import com.leo.algorithm.model.ListNode;
+
+/**
+ * https://leetcode-cn.com/problems/linked-list-cycle/
+ */
+public class HasCycle {
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+
+}
